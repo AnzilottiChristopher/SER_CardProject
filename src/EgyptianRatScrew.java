e428e4a74c4ac0d826c4e3bc2cca914012f9c8b7
Chris Anzilotti <anzilottichr@gmail.com>
2023-04-12 23:56:28 -0400

    Split the deck between two players

--- src/EgyptianRatScrew.java
+++ src/EgyptianRatScrew.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+
 public class EgyptianRatScrew 
 {    public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        //Creating lists that will hold cards
+        ArrayList<Card> player1 = new ArrayList<Card>();
+        ArrayList<Card> player2 = new ArrayList<Card>();
+        ArrayList<Card> middlePile = new ArrayList<Card>();
+
+        //Deck Size
+        final int deckSize = 52;
+
+        //Creating the deck and shuffling it
+        DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
+
+
+        for(int counter = 0; counter < deckSize; counter ++)
+        {
+            if(counter < 26)
+            {
+                player1.add(deck.dealCard());
+            }
+            else
+            {
+                player2.add(deck.dealCard());
+            }
+        }
     }
 }
