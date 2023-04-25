b4d4de5a780744198f1e368166c4d085727cf7ed
Chris Anzilotti <anzilottichr@gmail.com>
2023-04-24 22:49:35 -0400

    Changing things

--- src/Players.java
+++ src/Players.java
@@ -3,19 +3,28 @@ import java.util.ArrayList;
 public class Players 
 {
     //Creating lists that will hold cards
-    private ArrayList<Card> player1 = new ArrayList<Card>();
-    private ArrayList<Card> player2 = new ArrayList<Card>();
-    private ArrayList<Card> middlePile = new ArrayList<Card>();
+    private ArrayList<Card> player1;
+    private ArrayList<Card> player2;
+    private DeckOfCards deck = new DeckOfCards();
+    private final static int deckSize = 52;
 
     //Creating the deck and shuffling it
     public Players()
     {
+        deck.shuffle();
+        this.player1  = new ArrayList<Card>(deckSize/2);
+        this.player2 = new ArrayList<Card>(deckSize/2);
     }
 
-    public Players(DeckOfCards deck)
+    public void players()
     {
-
+        for(int counter = 0; counter < deckSize/2; counter ++)
+        {
+            if(counter < deckSize/2)
+            {
+                this.player1.add(deck.dealCard());
+            }
+        }
     }
-      
     
 }
