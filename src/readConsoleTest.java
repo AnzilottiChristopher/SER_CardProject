9442a465bcdccc5146e0a90c8263c62aadacc8f1
Chris Anzilotti <anzilottichr@gmail.com>
2023-04-24 23:52:53 -0400

    Failed Keyboard

---
+++ src/readConsoleTest.java
@@ -0,0 +1,11 @@
+public class readConsoleTest 
+{
+
+    //! Can't find easy way to get immediate input while testing in VSCode (IDE's) which could mean that it only doesn't work within IDE
+    
+    //Todo Figure out how to get terminal to immediately read input
+
+                                //Or
+                                
+    //Todo Figure out how to get Swing Working 
+}
