3208e6b572b9607a858ef4b80a69778a0a457656
Chris Anzilotti <anzilottichr@gmail.com>
2023-04-26 21:50:32 -0400

    back and forth logic not working

--- src/EgyptianRatScrew.java
+++ src/EgyptianRatScrew.java
@@ -53,11 +53,18 @@ public class EgyptianRatScrew {
          * }
          */
 
+         System.out.println();
+         System.out.println("Player 1 begins:");
+
         // Game logic starts here
         while (!player1.isWin() && !player2.isWin()) {
 
+            boolean player1CardPlace = true;
+            boolean player2CardPlace = false;
+
+            
             // Logic for when player1 places a card
-            if (gui.handleInput().equalsIgnoreCase((player1.getAddCardPress()))) {
+            if (gui.handleInput().equalsIgnoreCase((player1.getAddCardPress())) && player1CardPlace) {
                 // We may have to include logic that does not allow this when player 1 has no
                 // cards.
                 middlePile.addCard(player1.placeCard());
@@ -65,18 +72,26 @@ public class EgyptianRatScrew {
                 System.out.println("Player 1 placed " + middlePile.getLast());
                 // Here we call the print card function
 
+                player1CardPlace = false;
+                player2CardPlace = true;
+                System.out.println("here");
+
                 // Then program sleeps for a bit, so it doesnt register the input again
                 Thread.sleep(300);
             }
 
             // Logic for when player2 places a card
-            if (gui.handleInput().equalsIgnoreCase((player2.getAddCardPress()))) {
+            if (gui.handleInput().equalsIgnoreCase((player2.getAddCardPress())) && player2CardPlace) {
                 // We may have to include logic that does not allow this when player 2 has no
                 // cards.
                 middlePile.addCard(player2.placeCard());
                 // For now, print statement
                 System.out.println("Player 2 placed " + middlePile.getLast());
                 // Here we call the print card function
+
+
+                player1CardPlace = true;
+                player2CardPlace = false;
 
                 // Then program sleeps for a bit, so it doesnt register the input again
                 Thread.sleep(300);
